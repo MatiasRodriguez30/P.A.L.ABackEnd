6e57cf78c005750890f24bfe4a53a2e1ec85939e
MatiasRodriguez30 <119769021+MatiasRodriguez30@users.noreply.github.com>
2026-06-25 04:09:50 -0300

    feat: no lanzar excepcion si postulante/reclutador no tiene perfil local; detectar perfilPendiente para ambos roles

--- src/main/java/com/facultad/sistemaavisos/experiencia/Experiencia.java
+++ src/main/java/com/facultad/sistemaavisos/experiencia/Experiencia.java
@@ -29,9 +29,6 @@ public class Experiencia {
     private Long id;
 
     @Column(name = "descripcion_experiencia")
-    private String nombreExperiencia;
-
-    @Column(name = "descripcion_experiencia")
     private String descripcionExperiencia;
 
     @Column(name = "fecha_baja_experiencia")
